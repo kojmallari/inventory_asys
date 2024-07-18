a0b13dbeb3a6b4dd68810338dfa0c7f9ffd5ac27
Kenjie Mallari <83747226+kojmallari@users.noreply.github.com>
2024-07-18 09:22:54 +0800

    Add files via upload

--- inventory/inventory_menu.php
+++ inventory/inventory_menu.php
@@ -112,20 +112,6 @@ if (!isset($_SESSION['username'])) {
                         </a>
                     </div>
                 </div>
-                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="uploads/maintenance.avif" alt="id-card"/>
-                    </a>
-                    <div class="p-5">
-                        <h5 class="mb-7 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Maintenance Supplies</h5>
-                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Proceed to Inventory
-                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
             </div>
             <div class="flex justify-center space-x-4 mt-8">
                 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -149,20 +135,6 @@ if (!isset($_SESSION['username'])) {
                     <div class="p-5">
                         <h5 class="mb-7 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Printer Supplies</h5>
                         <a href="printersupps_view.php" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Proceed to Inventory
-                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="uploads/utility.avif" alt="id-card"/>
-                    </a>
-                    <div class="p-5">
-                        <h5 class="mb-7 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Miscellaneous Supplies</h5>
-                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Proceed to Inventory
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
